805703e805b585ed010f81e1d0067c09d986bee1
JMHere <jmendez@student.neumont.edu>
2025-07-01 10:19:04 -0600

    Bug fix

---
+++ Renderer/Renderer.cpp
@@ -0,0 +1,7 @@
+#include "Renderer.h"
+#include <iostream>
+
+void viper::PrintMessage()
+{
+	std::cout << "hello from dll v2.0!\n";
+}
--- Source/Game/Main.cpp
+++ Source/Game/Main.cpp
@@ -1,5 +1,8 @@
+#include "../Engine/Core/Math.h"
 #include <iostream>
+
 int main()
 {
 	std::cout << "Hello, World!\n";
+	std::cout << viper::pi << std::endl;
 }
