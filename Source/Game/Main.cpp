1d5e7f209531453e8637a86a07ec55b98b860bc2
JMHere <jmendez@student.neumont.edu>
2025-07-11 15:22:56 -0600

    Adding sound files and adding ms paint

--- Source/Game/Main.cpp
+++ Source/Game/Main.cpp
@@ -3,7 +3,9 @@
 #include "Math/Vector2.h"
 #include "Core/Time.h"
 #include <Renderer.h>
+#include "Input/InputSystem.h"
 
+#include <fmod.hpp>
 #include <SDL3/SDL.h>
 #include <iostream>
 #include <vector>
@@ -13,12 +15,23 @@ constexpr int SCREEN_WIDTH = 1280;
 constexpr int SCREEN_HEIGHT = 1024;
 
 int main(int argc, char* argv[]) {
+    // initialize engine systems
     viper::Time time;
 
     viper::Renderer renderer;
 
     renderer.Initialize();
     renderer.CreateWindow("Viper Engine", 1280, 1024);
+
+    viper::InputSystem input;
+    input.Initialize();
+
+    // create audio system
+    FMOD::System* audio;
+    FMOD::System_Create(&audio);
+
+    void* extradriverdata = nullptr;
+    audio->init(32, FMOD_INIT_NORMAL, extradriverdata);
 
     SDL_Event e;
     bool quit = false;
@@ -63,34 +76,84 @@ int main(int argc, char* argv[]) {
         point.blue = viper::random::getRandomInt(255);
         point.x = viper::random::getRandomInt(SCREEN_WIDTH);
         point.y = viper::random::getRandomInt(SCREEN_HEIGHT);
-	}
+	}*/
 
-	std::cout << lines[0].red << " " << lines[0].green << " " << lines[0].blue << std::endl;
+    FMOD::Sound* sound = nullptr;
+    audio->createSound("test.wav", FMOD_DEFAULT, 0, &sound);
 
-    int randomRed = viper::random::getRandomInt(255);
-    int randomGreen = viper::random::getRandomInt(255);
-    int randomBlue = viper::random::getRandomInt(255);*/
+    audio->playSound(sound, 0, false, nullptr);
 
+    std::vector<viper::vec2> points;
+
+    // MAIN LOOP
     while (!quit) {
+        time.Tick();
         while (SDL_PollEvent(&e)) {
             if (e.type == SDL_EVENT_QUIT) {
                 quit = true;
             }
         }
+        // update engine systems
+        input.Update();
+        audio->update();
 
+        // get input
+        /*if (input.GetKeyReleased(SDL_SCANCODE_A)) {
+            std::cout << "pressed\n";
+        }
+
+        if (input.GetMouseButtonDown(viper::InputSystem::MouseButton::Left)) {
+            std::cout << "mouse pressed\n";
+        }*/
+
+        // No space Check
+        if (input.GetMouseButtonDown(viper::InputSystem::MouseButton::Left)) {
+            points.push_back(input.GetMousePosition());
+            
+        }
+
+        
+        // 10 pixel check
+        if (input.GetMouseButtonDown(viper::InputSystem::MouseButton::Left)) {
+            viper::vec2 position = input.GetMousePosition();
+            if ((int)points.size() == 0) points.push_back(position);
+            else if ((position - points.back()).Length() > 10)points.push_back(position);
+
+        }
+
+        
+
+        
+        /*viper::vec2 mouse = input.GetMousePosition();
+        std::cout << mouse.x << " " << mouse.y << std::endl;*/
+
+        // draw
         renderer.SetColor(0, 0, 0);
         renderer.Clear();
+
+        /*for (int i = 0; i < (int)points.size() - 1; i++) {
+            renderer.SetColor(255, 0, 0);
+            std::cout << points[i].x << std::endl;
+            renderer.DrawLine(points[i].x, points[i].y, points[static_cast<std::vector<viper::vec2, std::allocator<viper::vec2>>::size_type>(i) + 1].x, points[static_cast<std::vector<viper::vec2, std::allocator<viper::vec2>>::size_type>(i) + 1].y);
+        }*/
+
+
+        for (int i = 0; i < (int)points.size() - 1; i++) {
+            renderer.SetColor(viper::random::getRandomInt(256), viper::random::getRandomInt(256), viper::random::getRandomInt(256));
+            renderer.DrawLine(points[i].x, points[i].y, points[i + 1].x, points[i + 1].y);
+        }
+
 
         viper::vec2 speed(-40.0f, 0.0f);
         float length = speed.Length();
 
-        for (auto& star : stars) {
+        /*for (auto& star : stars) {
             star += speed * time.GetDeltaTime();
             if (star[0] > 1280) star[0] = 0;
             if (star[0] < 0) star[0] = 1280;
             renderer.SetColor(viper::random::getRandomInt(256), viper::random::getRandomInt(256), viper::random::getRandomInt(256));
             renderer.DrawPoint(star.x, star.y);
-        }
+        }*/
         
         /*for (int i = 0; i < 100; i++) {
             renderer.SetColor(viper::random::getRandomInt(256), viper::random::getRandomInt(256), viper::random::getRandomInt(256));
